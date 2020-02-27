2cac568e0471a74476d43f0d05ba563ab16390fc
JoeMJ <joemitchelljones@gmail.com>
2020-02-27 13:02:37 +0000

    buildTree basic completed

--- Huffman_Proto/Source.cpp
+++ Huffman_Proto/Source.cpp
@@ -11,6 +11,13 @@ struct node {
 };
 
 struct nodeHeap {
+
+	nodeHeap(std::vector<char> input, std::vector<int> freq) {
+		for (int i = 0; i < input.size() - 1; ++i) {
+			nodeArr.push_back(newNode(input.at(i), freq.at(i)));
+		}
+	}
+
 	node* extractMin() {
 		std::vector<node*>::iterator minIt = std::min_element(nodeArr.begin(), nodeArr.end(), [](node* n1, node* n2) { return n1->freq_ < n2->freq_; });
 		node* tempN = *minIt;
@@ -20,6 +27,15 @@ struct nodeHeap {
 
 	bool isDone() {
 		return nodeArr.size() == 1;
+	}
+
+	void sortVector() {
+		std::sort(nodeArr.begin(), nodeArr.end(), [](node* n1, node* n2) { return n1->freq_ < n2->freq_; });
+	}
+
+	void insertNode(node* n) {
+		nodeArr.push_back(n);
+		sortVector();
 	}
 
 	std::vector<node*> nodeArr;
@@ -36,15 +52,22 @@ node* newNode(char input, int freq) {
 	return tempNode;
 }
 
-void initVector(std::vector<node*> &v, std::vector<char> input, std::vector<int> freq) {
-	for (int i = 0; i < input.size() - 1; ++i) {
-		v.push_back(newNode(input.at(i), freq.at(i)));
-	}
-}
-
 node* buildTree(std::vector<char> input, std::vector<int> freq) {
+	nodeHeap nh(input, freq);
 
 	node *left, *right, *top;
+
+	while (!nh.isDone) {
+		left = nh.extractMin();
+		right = nh.extractMin();
+		top = newNode('^', left->freq_ + right->freq_);
+
+		top->left = left;
+		top->right = right;
+
+		nh.insertNode(top);
+	}
+	return nh.extractMin();
 }
 
 int main() {
